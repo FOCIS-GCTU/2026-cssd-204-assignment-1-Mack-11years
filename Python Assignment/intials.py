f7faf20d7779f4bc61d2ea74d70bee94bcb8de41
Mack-11years <2425401780@live.gctu.edu.gh>
2026-05-30 05:32:23 +0000

    Add files via upload

---
+++ Python Assignment/intials.py
@@ -0,0 +1,35 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: OHENE JANEL DOMPREH
+# STUDENT ID: 2425401780
+# Email: 2425401780@live.gctu.edu.gh
+# Grader: Dr Kwame Eugene Afriyie
+#
+# On my honor, OHENE JANEL DOMPREH, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # This program prints my initials in stylized large letters.
+
+    print()
+    print("...OJD")
+    print()
+
+    print("...OOOOOOOOOOOO****JJJJJJJJJJJJ****DDDDDDDDDDDD")
+    print("...OO        OO****     JJ     ****DD        DD")
+    print("...OO        OO****     JJ     ****DD         DD")
+    print("...OO        OO****     JJ     ****DD         DD")
+    print("...OO        OO****     JJ     ****DD         DD")
+    print("...OO        OO****     JJ     ****DD         DD")
+    print("...OO        OO****JJ   JJ     ****DD         DD")
+    print("...OO        OO****JJ   JJ     ****DD         DD")
+    print("...OO        OO**** JJJJJ      ****DD        DD")
+    print("...OOOOOOOOOOOO********************DDDDDDDDDDDD")
+
+    print()
+
+
+main()
